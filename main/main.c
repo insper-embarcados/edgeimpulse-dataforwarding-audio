7dfb975a2a3c97bf518ef3c418d324d44d8c1a1b
danielmpinto <danielmessias.engenharia@gmail.com>
2026-05-21 17:05:18 -0300

    working

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
 #include "mpu6050.h"
 
 #include "Fusion.h"
@@ -17,64 +18,52 @@ const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
 
-static void mpu6050_init() {
-    i2c_init(i2c_default, 400 * 1000);
-    gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA_GPIO);
-    gpio_pull_up(I2C_SCL_GPIO);
 
-    // Two byte reset. First byte register, second byte data
-    // There are a load more options to set up the device in different ways that could be added here
-    uint8_t buf[] = {0x6B, 0x00};
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
+#define ADC_NUM 0
+#define ADC_PIN (26 + ADC_NUM)
+#define ADC_VREF 3.3
+#define ADC_RANGE (1 << 12)
+#define ADC_CONVERT (ADC_VREF / (ADC_RANGE - 1))
+
+volatile int g_timer_0 = 0;
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true; // keep repeating
 }
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
-    uint8_t buffer[14];
-
-    // Read all data sequentially starting from acceleration registers (0x3B)
-    // 0x3B-0x40: acceleration (6 bytes)
-    // 0x41-0x42: temperature (2 bytes)
-    // 0x43-0x48: gyro (6 bytes)
-    uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, MPU_ADDRESS, &val, 1, true);
-    i2c_read_blocking(i2c_default, MPU_ADDRESS, buffer, 14, false);
-
-    // Parse acceleration
-    for (int i = 0; i < 3; i++) {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
-    }
-
-    // Parse temperature
-    *temp = buffer[6] << 8 | buffer[7];
-
-    // Parse gyro
-    for (int i = 0; i < 3; i++) {
-        gyro[i] = (buffer[8 + i * 2] << 8 | buffer[8 + (i * 2) + 1]);
-    }
-}
-
-void mpu6050_task(void *p) {
-    mpu6050_init();
-
+void mic_task(void *p) {
+    uint adc_raw;
     while(1) {
-        int16_t acceleration[3], gyro[3], temp;
 
-        mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("%d, %d, %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        //printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        //printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        // vTaskDelay(62.5);  // 62.5 us delay for 16kHz sampling rate (1/16000 = 0.0000625 seconds)
+        // sleep_us(62.5); 
 
-        // 100 Hz
-        vTaskDelay(pdMS_TO_TICKS(10));
+
+        if(g_timer_0){
+            adc_raw = adc_read(); // raw voltage from ADC
+            printf("%.2f\n", adc_raw * ADC_CONVERT);
+            g_timer_0 = 0;
+        }
     }
 }
 
 int main() {
     stdio_init_all();
+    adc_init();
+    adc_gpio_init( ADC_PIN);
+    adc_select_input( ADC_NUM);
 
-    xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    repeating_timer_t timer_0;
+
+    if (!add_repeating_timer_us(62.5, 
+                                timer_0_callback,
+                                NULL, 
+                                &timer_0)) {
+        printf("Failed to add timer\n");
+    }
+
+    xTaskCreate(mic_task, "Mic_Task", 8192, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
